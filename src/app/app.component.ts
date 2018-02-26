3366754176105fff05de39d37b6fc9ab5ddd6116
luisor <luisorstaby@gmail.com>
2018-02-26 17:21:30 -0500

    Método

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,33 @@ import { Servicio1Service } from './servicio1.service';
 })
 export class AppComponent {
   title = 'app';
-  public var1= "yimmy y su primer proyecto en angular5";
-  public var2= "5";
-  public hoy=new Date();
-  public nombre="yimmy";
-  public numero =0
+  public var1 = "yimmy y su primer proyecto en angular5";
+  public var2 = "5";
+  public hoy = new Date();
+  public nombre = "yimmy";
+  public numero = 0
+  public n1 = 12;
+  public n2 = 0;
+  public mostrar:boolean = false;
 
-  constructor(public auth:Servicio1Service){}
+  constructor(public auth:Servicio1Service) {
+  }
 
-calcularfecha() {
-  var dia;
-  var mes;
-  var anio;
-  dia = this.hoy.getDate();
-  mes = this.hoy.getMonth();
-  anio = this.hoy.getFullYear();
-  console.log(this.hoy);
-}
-  obtenerservicio(){
-    this.numero=this.auth.getNumero()
+  calcularfecha() {
+    var dia;
+    var mes;
+    var anio;
+    dia = this.hoy.getDate();
+    mes = this.hoy.getMonth();
+    anio = this.hoy.getFullYear();
+    console.log(this.hoy);
+  }
+
+  obtenerservicio() {
+    this.numero = this.auth.getNumero()
+  }
+
+  mostrarObjeto() {
+    this.mostrar = !this.mostrar;
   }
 }
