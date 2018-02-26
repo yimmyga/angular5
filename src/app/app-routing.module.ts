29754aef718480dd2944ba23b7ab312e461e1032
luisor <luisorstaby@gmail.com>
2018-02-26 15:41:32 -0500

    Creación Componente6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { Componente2Component} from './componente2/componente2.component';
 import { Componente3Component} from './componente3/componente3.component';
 import { Componente4Component} from './componente4/componente4.component';
+import { Componente6Component} from './componente6/componente6.component';
 
 
 const routes: Routes = [
     { path: 'componente2' , component: Componente2Component },
     { path: 'componente3' , component: Componente3Component },
     { path: 'componente4' , component: Componente4Component },
+    { path: 'componente6' , component: Componente6Component },
     { path: '', redirectTo: '/', pathMatch: 'full' },
     { path: '**', component: AppComponent},
 
